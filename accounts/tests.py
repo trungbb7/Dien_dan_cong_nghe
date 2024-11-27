f06b91c790b19def082df8ce315f02abb2bd8d56
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2024-11-27 13:43:35 +0700

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 97ee8979c26f5f129f58b20f34ac71188cb18c3b
