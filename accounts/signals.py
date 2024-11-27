58a42578a93cb03f2fdc042ed58b9b1dfc4e7bf4
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2024-11-27 13:43:21 +0700

    Update signals.py

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -13,18 +12,4 @@ def create_user_profile(sender, instance, created, **kwargs):
             instance.profile.save()
         except Profile.DoesNotExist:
             Profile.objects.create(user=instance)
-=======
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Profile
-from django.contrib.auth.models import User
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
->>>>>>> 97ee8979c26f5f129f58b20f34ac71188cb18c3b
