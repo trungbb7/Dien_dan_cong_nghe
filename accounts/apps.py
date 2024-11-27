053ac53c02d6cab88f8fbf2bed3e97ecedd1be5f
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2024-11-27 13:42:08 +0700

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
@@ -6,12 +5,3 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         import accounts.signals
-=======
-from django.apps import AppConfig
-
-class AccountsConfig(AppConfig):
-    name = 'accounts'
-
-    def ready(self):
-        import accounts.signals
->>>>>>> 97ee8979c26f5f129f58b20f34ac71188cb18c3b
