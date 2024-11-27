30f13e6d91a6a523a50a6789beded4780bd9d94c
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2024-11-27 13:41:40 +0700

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
-# Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> 97ee8979c26f5f129f58b20f34ac71188cb18c3b
